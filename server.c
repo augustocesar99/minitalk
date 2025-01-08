4c4b98d6c8ac51a3e43a8e00be678fa3ac736406
Augusto Cesar Mendes Costa <acesar-m@c1r8p2.42sp.org.br>
2025-01-08 11:21:56 -0300

    new

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: acesar-m <acesar-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 17:21:44 by acesar-m          #+#    #+#             */
-/*   Updated: 2025/01/08 09:41:11 by acesar-m         ###   ########.fr       */
+/*   Updated: 2025/01/08 11:18:08 by acesar-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ void	ft_listening_data(int sig)
 	static int	bit;
 	static int	i;
 	char		*c1;
-
-	//4) Converta o sinal recebido em bytes -> caractere -> string.
+	//4) Converte o sinal recebido em bytes -> caractere -> string.
 	if (sig == SIGUSR1)
 		i |= (0x01 << bit);
 	bit++;
@@ -43,17 +42,19 @@ void	ft_listening_data(int sig)
 		free(c1);
 	}
 }
-
+	
 int	main(void)
 {
-	//Passo 1) Imprimir o PID do Servidor.
+	//Passo 1) Imprimi o PID do Servidor.
 	ft_putstr_fd("Server PID: [", 1);
 	ft_putnbr(getpid());
 	ft_putstr_fd("]\n", 1);
-	//3) Defina como cada sinal (SIGUSR1 e SIGUSR2) será tratado.
+	//3) Define como cada sinal (SIGUSR1 e SIGUSR2) será tratado.
 	signal(SIGUSR1, ft_listening_data);
 	signal(SIGUSR2, ft_listening_data);
-	//2) Mantenha o programa em execução, aguardando sinais para escutar.
-	while (1) 
+	//2) Mantem o programa em execução, aguardando sinais para escutar.
+	while (1)
+	{
 		pause();
+	}
 }
